1e41ba78cb16e1f96bf61e09b1984d614969e19a
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-07-29 19:46:31 +0530

    make map.html at startup

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
+import os
 from flask import Flask, render_template
+from generate import GenerateMap
 
 app = Flask(__name__)
+
+
+def makeMap():
+    obj = GenerateMap()
+    obj.run(path=os.path.join(os.getcwd(), 'templates', 'map.html'))
 
 
 @app.route("/")
 @app.route("/map")
 def index():
     return render_template("map.html")
+
+
+if __name__ == "__main__":
+    makeMap()
+    app.run("0.0.0.0", port=8090, debug=True)
