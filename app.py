7546f366ba7db0e45d3cd84bb534319dce7d0f7c
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-08-01 09:19:55 +0530

    backend to handle approvals and pendings

--- app.py
+++ app.py
@@ -86,6 +86,45 @@ def admin_approve():
     if request.method == 'POST':
         itemid = request.form.get('itemid')
         itemdo = request.form.get('itemdo')
+
+        if itemdo.lower() == 'add':
+            data_pending = Pending.getByID(id=int(itemid))
+            print("="*50)
+            print("Pending Data: \n", data_pending)
+
+            Stations.addStation(
+                name=data_pending['name'],
+                registration=data_pending['registration'],
+                phone=data_pending['phone'],
+                email=data_pending['email'],
+                coordinates=data_pending['coordinates'],
+                city=data_pending['city'],
+                availablitiy=data_pending['availablitiy']
+            )
+            print("="*50)
+            print("added data to Stations DB")
+
+            Pending.deleteByID(id=int(data_pending['id']))
+            del data_pending
+
+            print("="*50)
+            print("Deleted item from Pending DB")
+
+            return jsonify({'status': 'success'})
+
+        elif itemdo.lower() == 'remove':
+
+            Pending.deleteByID(id=int(itemid))
+
+            print("="*50)
+            print("Deleted item from Pending DB")
+
+            return jsonify({'status': 'success'})
+
+        else:
+            print(itemdo)
+            return "hi"
+
     else:
         data = {}
         data['pending'] = Pending.getAllStations()
