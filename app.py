7f845ee7e8b2649ef5dfd8ae85b4cd0c6962a9b4
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-08-02 21:58:27 +0530

    IT WORKED! I LOVE YOU @Larslol140

--- app.py
+++ app.py
@@ -236,8 +236,6 @@ def add_new_station():
     if not g.user:
         return redirect(url_for('login'))
 
-    print(len(request.form))
-
     # always
     fsname = request.form.get('fsname')
     fscity = request.form.get('fscity')
@@ -279,29 +277,27 @@ def add_new_station():
     # path = os.path.join(app.config['UPLOAD_FOLDER'], regProof.filename)
     # regProof.save(path)
 
-    # if 'regProof' not in request.files:
-    #     status['status'].append(
-    #         'Please upload the proof of registration and try again!')
-    # regProof = request.files['regProof']
-    # if regProof.filename == '':
-    #     status['status'].append(
-    #         'Please select a proper file')
-    # if regProof and allowed_file(regProof.filename):
-    #     savepath = os.path.join(
-    #         app.config['UPLOAD_FOLDER'],
-    #         str(secure_filename(regProof.filename)) + ''.join(
-    #             random.choice(
-    #                 string.ascii_letters + string.digits
-    #             ) for i in range(5)
-    #         )
-    #     )
-    #     regProof.save(savepath)
-
-    regProof = request.form.get('regProof')
-    print(regProof)
-    print(type(regProof))
-    print(request.files)
-    print(len(request.files))
+    if 'regProof' not in request.files:
+        status['status'].append(
+            'Please upload the proof of registration and try again!')
+    regProof = request.files['regProof']
+    if regProof.filename == '':
+        status['status'].append(
+            'Please select a proper file')
+    if regProof and allowed_file(regProof.filename):
+        savepath = os.path.join(
+            app.config['UPLOAD_FOLDER'],
+            ''.join(
+                random.choice(
+                    string.ascii_letters + string.digits
+                ) for i in range(5)
+            ) + str(
+                secure_filename(
+                    regProof.filename
+                )
+            )
+        )
+        regProof.save(savepath)
 
     if len(status['status']) != 0:
         return jsonify(status)
@@ -315,7 +311,7 @@ def add_new_station():
         city=fscity,
         petrol=True if petrolAvailability == '1' else False,
         diesel=True if dieselAvailability == '1' else False,
-        image="test",
+        image=savepath,
         lastupdated=datetime.now()
     )
 
