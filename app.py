05771c21811223c026992ac2a8a1ec0f94932cde
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-08-02 20:55:27 +0530

    get image to backend

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import os
+import random
+import string
 from datetime import datetime
 
 from flask import Flask
 from flask import jsonify, redirect, render_template, url_for
 from flask import g, request, session
+from werkzeug.utils import secure_filename
 
 from database.mongo import Pending, Stations, Users
 from database.settings import adminkey, flaskSecret
@@ -11,6 +14,13 @@ from generate import GenerateMap
 
 app = Flask(__name__)
 app.secret_key = flaskSecret
+app.config['UPLOAD_FOLDER'] = os.path.join(os.getcwd(), 'static', 'uploads')
+
+ALLOWED_EXTENSIONS = set(['pdf', 'png', 'jpg', 'jpeg'])
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 def makeMap():
@@ -261,6 +271,30 @@ def add_new_station():
     except IndexError:
         status['status'].append('Error processing the Google Maps URL')
 
+    # if 'regProof' not in request.files:
+    #     return status['status'].append('Please upload the proof of registration and try again!')
+    # regProof = request.files['regProof']
+    # path = os.path.join(app.config['UPLOAD_FOLDER'], regProof.filename)
+    # regProof.save(path)
+
+    if 'regProof' not in request.files:
+        status['status'].append(
+            'Please upload the proof of registration and try again!')
+    regProof = request.files['regProof']
+    if regProof.filename == '':
+        status['status'].append(
+            'Please select a proper file')
+    if regProof and allowed_file(regProof.filename):
+        savepath = os.path.join(
+            app.config['UPLOAD_FOLDER'],
+            str(secure_filename(regProof.filename)) + ''.join(
+                random.choice(
+                    string.ascii_letters + string.digits
+                ) for i in range(5)
+            )
+        )
+        regProof.save(savepath)
+
     if len(status['status']) != 0:
         return jsonify(status)
 
@@ -273,6 +307,7 @@ def add_new_station():
         city=fscity,
         petrol=True if petrolAvailability == '1' else False,
         diesel=True if dieselAvailability == '1' else False,
+        image=savepath,
         lastupdated=datetime.now()
     )
 
